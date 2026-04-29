229c9e0f0efd43a25e30a05c2df006cb197e9b02
AdelGann <imadelg14@gmail.com>
2026-04-29 09:12:17 -0400

    fix: tsup build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/cli.ts"],
-  format: ["cjs"],
+  format: ["cjs", "esm"],
   clean: true,
   dts: true,
   minify: true,
-  external: [],
+  platform: "node",
+  external: ["fs", "path"],
 });
