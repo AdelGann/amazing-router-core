863fd66d022d2188999f1b31ddb080b7ad734817
Adel Chafic Gannem <imadelg14@gmail.com>
2026-04-29 19:53:11 -0400

    fix: tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,4 +8,7 @@ export default defineConfig({
   minify: true,
   platform: "node",
   external: ["fs", "path"],
+  banner: {
+    js: "import { createRequire } from 'module'; import { fileURLToPath } from 'url'; import { dirname } from 'path'; const require = createRequire(import.meta.url); const __filename = fileURLToPath(import.meta.url); const __dirname = dirname(__filename);",
+  },
 });
